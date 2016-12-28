2b9e7a5eff912e16dc64600a07bbb563ea25f1c8
vintagewang <vintagewang@apache.org>
2016-12-28 16:45:25 +0800

    [ROCKETMQ-17] Develop a vendor-neutral open standard for distributed messaging: add message header const ASF JIRA: https://issues.apache.org/jira/browse/ROCKETMQ-17

---
+++ spec/code/messaging-user-level-api/java/src/main/java/org/apache/openmessaging/MessageHeaderConst.java
@@ -0,0 +1,31 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.openmessaging;
+
+
+public class MessageHeaderConst {
+    public static final String OMSTopic = "OMSTopic";
+    public static final String OMSBornTimestamp = "OMSBornTimestamp";
+    public static final String OMSBornHost = "OMSBornHost";
+    public static final String OMSStoreTimestamp = "OMSStoreTimestamp";
+    public static final String OMSStoreHost = "OMSStoreHost";
+    public static final String OMSStartTime = "OMSStartTime";
+    public static final String OMSStopTime = "OMSStopTime";
+    public static final String OMSTimeout = "OMSTimeout";
+    public static final String OMSPriority = "OMSPriority";
+    public static final String OMSReliability = "OMSReliability";
+}
